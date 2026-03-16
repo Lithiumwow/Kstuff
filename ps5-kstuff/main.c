2ae77de0d4f64a4d5b0b6b66d49a4a0edf0525bb
Lithiumwow <lithiumwow@users.noreply.github.com>
2026-03-16 17:23:27 -0400

    Update welcome message: Welcome to homebrew with Lithiumwow SquashFS support

--- ps5-kstuff/main.c
+++ ps5-kstuff/main.c
@@ -2854,7 +2854,7 @@ int main(void* ds, int a, int b, uintptr_t c, uintptr_t d)
     int minor_dec = (minor_bcd >> 4) * 10 + (minor_bcd & 0xF);
 
     char msg[64], *p = msg;
-    char *hdr = "Welcome To Kstuff 1.6.7\nPlayStation 5 FW: ";
+    char *hdr = "Welcome to homebrew ! original @sleirsgoevy mod by Lithiumwow to support Sqfs\nPlayStation 5 FW: ";
     while (*hdr) *p++ = *hdr++;
 
     if (major >= 10) *p++ = '0' + major / 10;
